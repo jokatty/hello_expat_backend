c45ae9ab3965fada8e191b12e9848b19d22623aa
jokatty <jyotikattani@gmail.com>
2022-05-04 17:54:37 +0200

    set app view engine to ejs

--- index.mjs
+++ index.mjs
@@ -1,11 +1,18 @@
 import expres from 'express';
+import bindRoutes from './routes.mjs';
 
 // create app from express instance
 const app = expres();
-// assign a port 
-const PORT = 3004
+// assign a port
+const PORT = 3004;
+
+// Set the Express view engine to expect EJS templates
+app.set('view engine', 'ejs');
+
+// bind route definations to the express application
+bindRoutes(app);
 
 // listen to the app at the port
-app.listen(PORT, ()=>{
-  console.log('app started on port', PORT)
-})
+app.listen(PORT, () => {
+  console.log('app started on port', PORT);
+});
