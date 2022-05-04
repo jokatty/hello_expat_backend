f85abd319acd342d512c05ac7c958bbdb0804af3
jokatty <jyotikattani@gmail.com>
2022-05-04 12:36:19 +0200

    add all  models to the index file

--- models/index.mjs
+++ models/index.mjs
@@ -1,16 +1,22 @@
 import sequelizePackage from 'sequelize';
-import allConfig from '../config/config.js';
+import allConfig from '../config/config';
 
-import initCategoryModel from './category.mjs';
+import initCategoryModel from './category';
+import initAnswerModel from './answer';
+import initQuestionModel from './question';
+import initExperienceModel from './experience';
 
 const { Sequelize } = sequelizePackage;
 const env = process.env.NODE_ENV || 'development';
 const config = allConfig[env];
 const db = {};
 
-let sequelize = new Sequelize(config.database, config.username, config.password, config);
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.Category = initCategoryModel(sequelize, Sequelize.DataTypes);
+db.Answer = initAnswerModel(sequelize, Sequelize.DataTypes);
+db.Question = initQuestionModel(sequelize, Sequelize.DataTypes);
+db.Experience = initExperienceModel(sequelize, Sequelize.DataTypes);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
